5cc7cffbfca2352c3adba60e4c6e3998f4241531
Nikolai <smirnovnikolai@yandex.ru>
2021-07-01 01:05:24 +0300

    Корректировка кода 1

--- homework.py
+++ homework.py
@@ -1,10 +1,10 @@
 import logging
 import os
 import time
+
 import requests
 import telegram
 from dotenv import load_dotenv
-
 
 load_dotenv()
 
@@ -12,53 +12,90 @@ PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
+HOMEWORK_STATUS = {
+    'reviewing': 'Работа взята в ревью.',
+    'approved': 'Ревьюеру всё понравилось, работа зачтена!',
+    'rejected': 'К сожалению, в работе нашлись ошибки.'
+}
+TIMER = 20
+
+logging.basicConfig(
+    level=logging.INFO,
+    filename='bot.log',
+    format='%(asctime)s, %(levelname)s, %(message)s, %(name)s'
+)
 
 bot = telegram.Bot(token=f'{TELEGRAM_TOKEN}')
 
 
+class NegativeValueException(Exception):
+    pass
+
+
 def parse_homework_status(homework):
-    homework_name = homework['homework_name']
-    status = homework['status']
-    if status != 'approved':
-        verdict = 'К сожалению, в работе нашлись ошибки.'
-    else:
-        verdict = 'Ревьюеру всё понравилось, работа зачтена!'
-    return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+    try:
+
+        if 'homework_name' not in homework:
+            raise NegativeValueException('Не найдено имя домашней работы!')
+        if 'status' not in homework:
+            raise NegativeValueException('Не найден статус домашней работы!')
+
+        homework_name = homework.get('homework_name')
+        status = homework.get('status')
+
+        if status not in HOMEWORK_STATUS:
+            raise NegativeValueException(
+                f'Домашняя работа вернулась с неизвестным статусом {status}'
+            )
+
+        if status in HOMEWORK_STATUS:
+            verdict = HOMEWORK_STATUS[status]
+        return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+
+    except NegativeValueException as e:
+        logging.exception(f'Получена некорректная информация: {e}')
+        raise
 
 
 def get_homeworks(current_timestamp):
     headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
     payload = {'from_date': current_timestamp}
-    homework_statuses = requests.get(URL, headers=headers, params=payload)
-    return homework_statuses.json()
+    try:
+        homework_statuses = requests.get(URL, headers=headers, params=payload)
+        try:
+            return homework_statuses.json()
+        except Exception:
+            logging.exception('Возникла ошибка при распаковке JSON')
+    except requests.exceptions.RequestException as e:
+        logging.exception(f'Возникла ошибка {e} при соединении с сервером')
+        raise
 
 
 def send_message(message):
+    logging.info('Сообщение отправлено')
     return bot.send_message(chat_id=CHAT_ID, text=message)
 
 
 def main():
-    current_timestamp = int(time.time())
-
+    logging.info('Бот запущен')
     while True:
         try:
-            logging.basicConfig(
-                level=logging.DEBUG,
-                filename='bot.log',
-                format='%(asctime)s, %(levelname)s, %(message)s, %(name)s'
-            )
-            homework = get_homeworks(current_timestamp)
-            message = parse_homework_status(homework['homeworks'][0])
-            logging.info('Send message')
-            send_message(message)
-            logging.info('Message sent')
-            time.sleep(20 * 60)
+            try:
+                current_timestamp = int(time.time())
+                homework = get_homeworks(current_timestamp)
+                if homework.get('homeworks'):
+                    message = parse_homework_status(homework['homeworks'][0])
+                    send_message(message)
+                time.sleep(TIMER)
 
-        except Exception as e:
-            logging.exception(f'Бот упал с ошибкой: {e}')
-            send_message(f'Бот упал с ошибкой: {e}')
-            time.sleep(20)
-            continue
+            except Exception as e:
+                logging.exception(f'Бот упал с ошибкой: {e}')
+                send_message(f'Бот упал с ошибкой: {e}')
+                time.sleep(TIMER)
+                continue
+        except KeyboardInterrupt:
+            break
+    logging.info('Бот отключён')
 
 
 if __name__ == '__main__':
